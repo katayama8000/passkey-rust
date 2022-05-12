198000ac7b65d096371d5549d04a49770eaa9518
katayama <tattu.0310@gmail.com>
2022-05-12 17:40:17 +0900

    🔧 @componentで追加可能

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,15 @@
 import type { NextPage } from "next";
 import { Button } from "src/lib/mantine/Button";
-import { useMediaQuery } from "src/lib/mantine/useMediaQuery";
-import { useViewportSize } from "src/lib/mantine/useViewportSize";
+import { Sample } from "@component/Sample";
 
 const handleClick = () => {
   console.log("Hello!");
 };
 
 const Home: NextPage = () => {
-  const { width } = useViewportSize();
-  const largerThanXs = useMediaQuery("xs");
-  const largerThanSm = useMediaQuery("sm");
-  const largerThanMd = useMediaQuery("md");
-  const largerThanLg = useMediaQuery("lg");
-  const largerThanXl = useMediaQuery("xl");
-
   return (
     <div className="p-20">
-      <div className="bg-fuchsia-200 xs:bg-red-200 sm:bg-amber-200 md:bg-lime-200 lg:bg-emerald-200 xl:bg-cyan-200">
-        <div>{`width: ${width}`}</div>
-        <div>{`largerThanXs: ${largerThanXs}`}</div>
-        <div>{`largerThanSm: ${largerThanSm}`}</div>
-        <div>{`largerThanMd: ${largerThanMd}`}</div>
-        <div>{`largerThanLg: ${largerThanLg}`}</div>
-        <div>{`largerThanXl: ${largerThanXl}`}</div>
-      </div>
+      <Sample />
       <Button dent onClick={handleClick} className="mt-4 block">
         Click me!
       </Button>
