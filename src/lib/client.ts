c2c5f7e6e03e49525a73521b6c01a6a07d7f36d6
katayama <tattu.0310@gmail.com>
2022-05-13 20:06:38 +0900

    ⚡️ 環境変数設定

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "katayama-blog",
-  apiKey: "0d13f221aa434f0db438ecaadbf45d7339cb",
+  apiKey: process.env.API_KEY,
 });
