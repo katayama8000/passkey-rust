e63c3df81792eaca5574db16d438187b5ecbdd5a
katayama <tattu.0310@gmail.com>
2022-05-14 20:42:38 +0900

    ⚡️ formをmantineに変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { MicroCMSListResponse } from "microcms-js-sdk";
 import { client } from "src/lib/client";
 //mantine
 import { Badge, Grid } from "@mantine/core";
+import { TextInput, Checkbox, Button, Group, Box } from "@mantine/core";
+import { useForm } from "@mantine/form";
 //component
 import { MainImageSwiper } from "@component/MainImageSwiper";
 import { BlogComponent } from "@component/BlogComponent";
@@ -22,9 +24,8 @@ const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
   const [search, setSearch] = useState<MicroCMSListResponse<Blog>>();
   const [allTags, setAlltags] = useState<string[]>([]);
 
-  const handleSubmit: ComponentProps<"form">["onSubmit"] = async (event) => {
-    event.preventDefault();
-    const q = event.currentTarget.query.value;
+  const handleSubmit = async (value: { search: string | number }) => {
+    const q = value.search;
     const data = await fetch("/api/search", {
       method: "POST",
       headers: { "Content-type": "application/json" },
@@ -33,8 +34,6 @@ const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
     const json: MicroCMSListResponse<Blog> = await data.json();
     setSearch(json);
   };
-  console.log(search);
-  console.log("全データ", props);
 
   const handleClick: ComponentProps<"button">["onClick"] = async () => {
     setSearch(undefined);
@@ -58,43 +57,61 @@ const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
   const contents = search ? search.contents : props.contents;
   const totalCount = search ? search.totalCount : props.totalCount;
 
+  const form = useForm({
+    initialValues: {
+      search: "",
+    },
+  });
+
   return (
     <div className="mx-auto max-w-6xl">
-      <form className="mt-2 flex gap-x-2" onSubmit={handleSubmit}>
-        <input type="text" name="query" className="border border-black px-2" />
-        <button className="border border-black px-2">検索</button>
-        <button
-          type="reset"
-          className="border border-black px-2"
-          onClick={handleClick}
+      <div>
+        <form
+          onSubmit={form.onSubmit((values) => handleSubmit(values))}
+          className="mt-2 flex gap-x-2"
         >
-          リセット
-        </button>
-      </form>
+          <TextInput
+            placeholder="searching..."
+            {...form.getInputProps("search")}
+          />
+          <Button type="submit">検索</Button>
+          <Button type="reset" onClick={handleClick}>
+            リセット
+          </Button>
+        </form>
+      </div>
       <p className="my-3 py-3 text-gray-500">{`${
         search ? "検索結果" : "記事の総数"
-      }:${totalCount}件`}</p>
+      } : ${totalCount}件`}</p>
       <Grid>
         <Grid.Col span={9}>
           <div>
             <MainImageSwiper />
           </div>
-          {contents.map((content) => {
-            return (
-              <div key={content.id}>
-                <Link href={`/blog/${content.id}`}>
-                  <a className="no-underline">
-                    <BlogComponent
-                      title={content.title}
-                      content={content.description}
-                      image={content.image}
-                      tags={content.tag}
-                    />
-                  </a>
-                </Link>
-              </div>
-            );
-          })}
+          {contents.length === 0 ? (
+            <div className="mt-5 flex w-full justify-center">
+              <p className="align-center rounded border border-cyan-300 p-5 text-cyan-500">
+                記事が見つかりませんでした。
+              </p>
+            </div>
+          ) : (
+            contents.map((content) => {
+              return (
+                <div key={content.id} className="pt-2">
+                  <Link href={`/blog/${content.id}`}>
+                    <a className="no-underline">
+                      <BlogComponent
+                        title={content.title}
+                        content={content.description}
+                        image={content.image}
+                        tags={content.tag}
+                      />
+                    </a>
+                  </Link>
+                </div>
+              );
+            })
+          )}
         </Grid.Col>
         <Grid.Col span={3}>
           <div className="p-2">
