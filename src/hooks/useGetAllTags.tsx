4765cf60dca074f0ec28218e2ff03787fa524457
katayama <tattu.0310@gmail.com>
2022-05-15 12:18:05 +0900

    ♻️ リファクタリング

--- src/hooks/useGetAllTags.tsx
+++ src/hooks/useGetAllTags.tsx
@@ -1,18 +1,10 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from "react";
-
 export const useGetAllTags = (contents: any) => {
-  const [allTags, setAlltags] = useState<string[]>([]);
-  useEffect(() => {
-    let array: string[] = [];
-    for (let i = 0; i < contents?.length; i++) {
-      array.push(...contents[i].tag);
-    }
-    const filteredArray = array.filter(function (ele, pos) {
-      return array.indexOf(ele) == pos;
-    });
-    setAlltags(filteredArray);
-  }, []);
-
+  let array: string[] = [];
+  for (let i = 0; i < contents?.length; i++) {
+    array.push(...contents[i].tag);
+  }
+  const allTags = array.filter((ele, pos) => {
+    return array.indexOf(ele) == pos;
+  });
   return allTags;
 };
